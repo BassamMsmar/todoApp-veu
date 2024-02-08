4abafa9888b5093fd756b2be73dc35192c477969
Bassam Msmar <bassammsmar@gmail.com>
2024-02-08 16:41:11 +0300

    add edit methods

--- src/components/todoApp.vue
+++ src/components/todoApp.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <h2>Todo App</h2>
     <div class="d-flex mt-5">
-      <input type="text" placeholder="Enter Task" class="form-control" v-model="newTask" />
+      <input type="text" placeholder="Enter Task" class="form-control" v-model="task" />
       <button type="button" class="btn btn-warning ms-3" @click="submitTask">Add</button>
     </div>
     <div class="mt-5">
@@ -21,12 +21,12 @@
             <th scope="row">{{task.name}}</th>
             <td>{{task.status}}</td>
             <td>
-              <div>
+              <div @click="editTask(index)">
                 <samp><i class="fa-regular fa-pen-to-square"></i></samp>
               </div>
             </td>
             <td>
-              <div>
+              <div @click="deleteTask(index)"> 
                 <samp><i class="fa-regular fa-trash-can"></i></samp>
               </div>
             </td>
@@ -45,7 +45,7 @@ export default {
   },
   data() {
     return {
-      newTask: '',
+      task: '',
       tasks : [
         {
           name: 'Stady js',
@@ -65,10 +65,18 @@ export default {
 
   methods: {
     submitTask() {
-      if (this.newTask === '') return
-      this.tasks.push({name: this.newTask, status: 'todo'})
+      if (this.task == '') return
+      this.tasks.push({name: this.task, status: 'todo'})
+      this.task = ''
       
     },
+    deleteTask(index) {
+      this.tasks.splice(index, 1)
+    },
+
+    editTask(index) {
+      this.task = this.tasks[index].name
+    }
 
   }
 
